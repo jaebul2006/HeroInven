0cc06a78f23961662780ac24f7e8cc8938fafd87
jangdongho <jaebul2006@naver.com>
2017-06-07 03:27:11 +0900

    bug fix

--- Assets/Scripts/GameDB.cs
+++ Assets/Scripts/GameDB.cs
@@ -16,8 +16,8 @@ public class GameDB : MonoBehaviour {
 		_itemList.Add (i0);
 
 		Item i1 = new Item ();
-		i1._name = "Arrow Quiver 2";
-		i1._type = Item.Type.equip;
+		i1._name = "Potion A 1";
+		i1._type = Item.Type.consumable;
 		i1._sprite = _sprites [1];
 		_itemList.Add (i1);
 
@@ -28,8 +28,8 @@ public class GameDB : MonoBehaviour {
 		_itemList.Add (i2);
 
 		Item i3 = new Item ();
-		i3._name = "Axe A 2";
-		i3._type = Item.Type.equip;
+		i3._name = "Potion A 2";
+		i3._type = Item.Type.consumable;
 		i3._sprite = _sprites [3];
 		_itemList.Add (i3);
 
